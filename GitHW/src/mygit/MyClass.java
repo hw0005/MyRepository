9d99850016bb32b549baa18f57fddb624538d26d
HYUNWOO\hw000 <hw0005@naver.com>
2025-12-04 14:22:17 +0900

    dev branch에서 수정

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("MyClass 수정");
+		System.out.println("3rd version"); // dev브랜치 생성
 	}
 }
