9a6b82575059baf860c69c6bb937e24b4fdb3540
HYUNWOO\hw000 <hw0005@naver.com>
2025-12-04 14:27:02 +0900

    Change in main branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -4,6 +4,7 @@ public class YourClass {
 
 	public static void main(String[] args) {
 		System.out.println("Say Hello"); // 클래스 추가(commit and Push) 확인용
+		System.out.println("YourClass를 main브랜치에서 수정함");
 	}
 
 }
