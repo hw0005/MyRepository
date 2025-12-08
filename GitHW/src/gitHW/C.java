e6b67e82746ed7b32001b053ec1b5d814759193d
HYUNWOO\hw000 <hw0005@naver.com>
2025-12-08 16:27:36 +0900

    modify C in master Branch

---
+++ GitHW/src/gitHW/C.java
@@ -0,0 +1,12 @@
+package gitHW;
+
+public class C {
+	public static void main(String[] args) {
+//		System.out.println("master branch에서 변경");
+//		System.out.println("Alice's change in C class");
+		System.out.println("Alice branch ->master branch에서 변경");
+//		A a = new A();
+//		System.out.println("Bob changed C class.");
+//		B b = new B();
+	}
+}
